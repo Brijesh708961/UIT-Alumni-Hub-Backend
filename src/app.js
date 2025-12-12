1c6b4f0d7f1a19e4e0f08fc5f9fa1d5f80bde668
anubhav0914 <anubhavg.cse22@gmail.com>
2025-12-12 16:11:28 +0530

    fixing auth middleware and making prisma modular

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// CORS setup
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*", // fallback to "*" if not set
+    credentials: true,
+  })
+);
+
+// Body parsers
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Static files
+app.use(express.static("public"));
+
+// Cookie parser
+app.use(cookieParser());
+
+// --- Routes Import ---
+import userRouter from "./router/user.routes.js";
+
+// --- Routes Declaration ---
+app.use("/api/v1/users", userRouter);
+
+export { app };
